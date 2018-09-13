f0294857b74f8ed56b8023f78b0be74d3a30fecf
Mark King <king2nd23@gmail.com>
2018-09-12 20:58:29 -0400

    Select goals button is functional

--- main.js
+++ main.js
@@ -1,73 +1,65 @@
-//
-// var selectSex = document.querySelector('#loose')
-// console.log(selectSex);
-//
-// var sexClick = 0;//counts button clicks
-// selectSex.onclick = function () {
-//   sexClick = sexClick + 1;
-//
-//   if (sexClick % 2 === 0) {
-//     selectSex.style.color = 'black'
-//     selectSex.style.backgroundColor = 'white'
-//   }
-//   else {
-//     selectSex.style.color = 'white'
-//     selectSex.style.backgroundColor = 'black'
-//   }
-//
-// };
 
-var myGoal = document.querySelectorAll(".selectGoals");
-console.log(myGoal.length);
 
-// myGoal[1].classList.add('selected');//works
-
-// var clickCounter = 0; //counts clicks
-//
-// function selectMyGoal() {
-//   // clickCounter = clickCounter + 1;
-//   // myGoal[i].classList.add('selected');
+// var myGoal = document.querySelectorAll(".selectGoals");
 //
 //   for (var i = 0; i < myGoal.length; i++) {
-//     myGoal[i].addEventListener('click', selectMyGoal);
-//     myGoal[i].classList.add('selected');
+//     var goalKey = i;
+//
+//     myGoal[goalKey].addEventListener('click', function (e) {
+//
+//
+//       var target = e.target;
+//       text = target.textContent;
+//
+//
+//       var clicked = this;
+//       this.classList.add('selected'); //this class adds black background white text coloe
+//       var goalNum = this.getAttribute("data-value");//get data value from clicked button
+//
+//       console.log(text);
+//     })
 //   }
+//
+//
+// // var loose = document.getElementById('loose');
+// // var gain = document.getElementById('gain');
+// // var maintain = document.getElementById('maintain');
+// var goalArray = document.querySelectorAll('.selectGoals');
+//
+//
+// for (var i = 0; i < goalArray.length; i++) {
+//   var goalNum = i
+//
+// goalArray[goalNum]
 // }
 
-//test
+
+document.addEventListener('click', function (e) {
+  e = e || window.event; //the "e" references the event obkect, "window.event is for cross browser capatibility(IE)"
+  var target = e.target || e.srcElement; //target gets the element n which the event occured srcElement is a IE non-standard way to obtain the target
+  text = target.textContent; //textContent is literally the text content inside of the element
+
+  if (target.id.substr(0,5)=="goals") {
+    //remove all class selected
+    var selecteds = document.getElementsByClassName('selected');
+    while (selecteds.length)
+      selecteds[0].classList.remove('selected');
+      //add class to the target
+      target.classList.add( 'selected');
+  };
+}false);
 
 
 
-  for (var i = 0; i < myGoal.length; i++) {
-    // var counter = i;
-    var goalKey = i;
-
-    myGoal[goalKey].addEventListener('click', function (e) {
-    e.target.classList.add('selected');
-
-    var goalKey = e.target.getAttribute("data-value");//get data value from clicked button
-
-    if (myGoal[0].classList.contains('selected')) {
-      // myGoal[1].classList.remove('selected');
-      // myGoal[2].classList.remove('selected');
-      return;
-    }
-
-    if (myGoal[1].classList.contains('selected')) {
-      // myGoal[0].classList.remove('selected');
-      // myGoal[2].classList.remove('selected');
-    }
-
-    if (myGoal[2].classList.contains('selected')) {
-      // myGoal[0].classList.remove('selected');
-      // myGoal[1].classList.remove('selected');
-    }
-
-    })
-  }
 
 
-// console.log(myGoal);
+
+
+
+
+
+
+
 
 
 
